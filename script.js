1410b3c5f0013f88d0b3396908b8d8b4ac257db5
ashutosh14300 <ashut2434@gmail.com>
2024-09-20 22:41:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menuDropdown = document.querySelector('.menu-dropdown');
 
-    menuToggle.addEventListener('click', () => {
-        menuDropdown.style.display = menuDropdown.style.display === 'block' ? 'none' : 'block';
-    });
-});
